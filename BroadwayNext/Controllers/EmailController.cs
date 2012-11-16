4bfb90cb254eb41db965191554a46d2147934638
Dipankar Biswas <dipankar_cse@yahoo.com>
2012-11-16 15:29:14 +0600

    Email Controller Added

---
+++ BroadwayNext/Controllers/EmailController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BroadwayNextWeb.Controllers
+{
+
+    public class NewEmailModel
+    {
+        public string TmpDir { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string CC { get; set; }
+        public string BCC { get; set; }
+        public string Subject { get; set; }
+        [AllowHtmlAttribute]
+        public string Body { get; set; }
+        public bool CCSender { get; set; }
+        public void ReplaceNullWithEmpty()
+        {
+            if (TmpDir == null) TmpDir = String.Empty;
+            if (From == null) From = String.Empty;
+            if (To == null) To = String.Empty;
+            if (CC == null) CC = String.Empty;
+            if (BCC == null) BCC = String.Empty;
+            if (Subject == null) Subject = String.Empty;
+            if (Body == null) Body = String.Empty;
+        }
+    }
+    public class EmailController : Controller
+    {
+        //
+        // GET: /Email/
+        private const string attachmentdir = "~/App_Data/";
+
+        public ActionResult NewEmail(NewEmailModel model)
+        {
+            return View(model);
+        }
+
+        public JsonResult GetAttachments(string emailDirectory)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Server.MapPath(attachmentdir + emailDirectory));
+            FileInfo[] files = dir.GetFiles();
+            List<String> fileNames = new List<string>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileNames.Add(files[i].Name);
+            }
+            return Json(fileNames, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult SendEmail(NewEmailModel mail)
+        {
+            mail.ReplaceNullWithEmpty();
+
+            string[] tos = mail.To.Split(new string[] { ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] ccs = mail.CC.Split(new string[] { ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] bccs = mail.BCC.Split(new string[] { ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(mail.From);
+            for (int i = 0; i < tos.Length; i++)
+            {
+                message.To.Add(tos[i]);
+            }
+            for (int i = 0; i < ccs.Length; i++)
+            {
+                message.CC.Add(ccs[i]);
+            }
+            for (int i = 0; i < bccs.Length; i++)
+            {
+                message.Bcc.Add(bccs[i]);
+            }
+            if (mail.CCSender) message.CC.Add(mail.From);
+
+            message.Subject = mail.Subject;
+            message.Body = mail.Body;
+            message.IsBodyHtml = true;
+
+            //add attachments
+            DirectoryInfo dir = new DirectoryInfo(Server.MapPath(attachmentdir + mail.TmpDir));
+            FileInfo[] files = dir.GetFiles();
+            for (int i = 0; i < files.Length; i++)
+            {
+                message.Attachments.Add(new Attachment(files[i].FullName));
+            }
+
+            //send email using smtp
+            SmtpClient smtpServer = new SmtpClient(ConfigurationManager.AppSettings["smtpServer"], Int32.Parse(ConfigurationManager.AppSettings["smtpPort"]));
+            smtpServer.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["smtpUserName"], ConfigurationManager.AppSettings["smtpPassword"]);
+            smtpServer.EnableSsl = Boolean.Parse(ConfigurationManager.AppSettings["smtpEnableSSL"]);
+            smtpServer.Send(message);
+
+            //cleanup tmp dir
+            dir.Delete(true);
+
+            return Json(new { Success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult EmailLookup()
+        {
+            return View();
+        }
+    }
+}
---
+++ BroadwayNext/Scripts/app/email.js
@@ -0,0 +1,120 @@
+﻿bn.vmVendorList.to = ko.observable();
+bn.vmVendorList.cc = ko.observable();
+bn.vmVendorList.bcc = ko.observable();
+bn.vmVendorList.pageSize(5);
+
+bn.vmVendorList.showEmailFinder = ko.observable(false);
+
+amplify.subscribe("VendorSelectionChanged", function (vID, vNum) {
+    console.log(bn.vmVendorList.selectedVendor());
+});
+
+amplify.subscribe("emaillookup", function (data) {
+    console.log(data);
+});
+
+ko.applyBindings(bn.vmVendorList);
+
+function loadFormData() {
+    bn.vmVendorList.to($('#hdnTO').val());
+    bn.vmVendorList.cc($('#hdnCC').val());
+    bn.vmVendorList.bcc($('#hdnBCC').val());
+}
+function openEmailLookup() {
+    bn.vmVendorList.loadVendors();
+    $('#toLookup').val(bn.vmVendorList.to());
+    $('#ccLookup').val(bn.vmVendorList.cc());
+    $('#bccLookup').val(bn.vmVendorList.bcc());
+    $('#emailLookupModal').modal();
+}
+$(document).ready(function () {
+    loadFormData();
+    $('#btnTO').click(function () {
+        openEmailLookup();
+    });
+    $('#btnCC').click(function () {
+        openEmailLookup();
+    });
+    $('#btnBCC').click(function () {
+        openEmailLookup();
+    });
+    $('#mailbody').wysihtml5({
+        "stylesheets": ["../Content/wysihtml5/lib/css/wysiwyg-color.css"]
+    });
+
+    $('#btnSend').click(function () {
+        var data = {
+            From: $('#hdnFrom').val(),
+            To: $('#to').val(),
+            CC: $('#cc').val(),
+            BCC: $('#bcc').val(),
+            Subject: $('#subject').val(),
+            Body: $('#mailbody').val(),
+            TmpDir: $('#hdnTmpDir').val(),
+            CCSender: $('#ccsender').val()
+        };
+        $.ajax({
+            url: '/email/sendemail',
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+                if (result.success) { alert('email sent'); }
+            }
+        });
+    });
+    $('#btnPrintPreview').click(function () {
+        previewHtml = '';
+        previewHtml += 'To: ' + $('#to').val() + '<br/>';
+        if (jQuery.trim($('#cc').val()).length > 0) previewHtml += 'CC: ' + + '<br/>';
+        if (jQuery.trim($('#bcc').val()).length > 0) previewHtml += 'BCC: ' + $('#bcc').val() + '<br/>';
+        if (jQuery.trim($('#subject').val()).length > 0) previewHtml += '<br/>Subject: ' + $('#subject').val() + '<br/><br/>';
+        previewHtml += $('#mailbody').val();
+        var w = window.open("Print Preview", "#");
+        var d = w.document.open();
+        d.write('<!DOCTYPE html><html><body>' + previewHtml + '</body></html>');
+        d.close();
+    });
+    $('#btnAddAttachments').click(function () {
+        alert('Not Implemented');
+    });
+    $('#btnRemoveAttachments').click(function () {
+        alert('Not Implemented');
+    });
+
+    $('#btnAddLookupToMainEmail').click(function () {
+        bn.vmVendorList.to($('#toLookup').val());
+        bn.vmVendorList.cc($('#ccLookup').val());
+        bn.vmVendorList.bcc($('#bccLookup').val());
+        $("#emailLookupModal").modal("hide");
+    });
+    $('#btnToLookup').click(function () {
+        var email = bn.vmVendorList.selectedVendor().Email();
+        if ($.trim($('#toLookup').val()).length > 0) {
+            $('#toLookup').val($('#toLookup').val() + ', ' + email);
+        }
+        else {
+            $('#toLookup').val(email);
+        }
+    });
+    $('#btnCCLookup').click(function () {
+        var email = bn.vmVendorList.selectedVendor().Email();
+        if ($.trim($('#ccLookup').val()).length > 0) {
+            $('#ccLookup').val($('#ccLookup').val() + ', ' + email);
+        }
+        else {
+            $('#ccLookup').val(email);
+        }
+
+    });
+    $('#btnBCCLookup').click(function () {
+        var email = bn.vmVendorList.selectedVendor().Email();
+        if ($.trim($('#bccLookup').val()).length > 0) {
+            $('#bccLookup').val($('#bccLookup').val() + ', ' + email);
+        }
+        else {
+            $('#bccLookup').val(email);
+        }
+    });
+});
