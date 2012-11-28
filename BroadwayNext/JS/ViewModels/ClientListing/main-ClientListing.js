e5a718cb9f94c7c0e2f1270aa4074c0dc1226df1
Md Nasir Uddin <nahidinfo@yahoo.com>
2012-11-28 22:00:49 +0600

    Added vmClientDocumentList Link in index and main-ClientListing

--- BroadwayNext/JS/ViewModels/ClientListing/main-ClientListing.js
+++ BroadwayNext/JS/ViewModels/ClientListing/main-ClientListing.js
@@ -7,4 +7,4 @@ ko.applyBindings(bn.vmClientList);
 //ko.applyBindings(bn.vmClientInstructionList, document.getElementById('#ClientTabs-7'));
 ko.applyBindings(bn.vmClientBillTo, document.getElementById("ClientTabs-5"));
 ko.applyBindings(bn.vmClientInstructionList, document.getElementById("ClientTabs-7"));
-//ko.applyBindings(bn.vmClientDocumentList, document.getElementById("ClientTabs-9"));
+ko.applyBindings(bn.vmClientDocumentList, document.getElementById("ClientTabs-9"));
