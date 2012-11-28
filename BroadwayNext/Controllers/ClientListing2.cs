d8253a025a46d8ccad53a3bdd57faeb56e4f1ef2
Chowdhury M Rahman <CMRahman@gmail.com>
2012-11-28 15:14:23 +0600

    Added Partial Class for ClientListing controller

---
+++ BroadwayNext/Controllers/ClientListing2.cs
@@ -0,0 +1,115 @@
+﻿using BroadwayNextWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BroadwayNextWeb.Controllers
+{
+    public partial class ClientListingController : Controller
+    {
+        #region Client
+
+        public JsonResult GetClients()
+        {
+            int totalRowCount;
+
+            using (UoW)
+            {
+                var clients = UoW.Clients.Get(out totalRowCount);
+                return Json(new { Data = clients, VirtualRowCount = totalRowCount }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+
+        public JsonResult GetAllClients1(int pageSize, int currentPage, string searchStr)
+        {
+            int totalRowCount;
+            Expression<Func<Vendor, bool>> searchFilter = null;
+
+            if (!string.IsNullOrEmpty(searchStr))
+            {
+                int vendorNum; bool result;
+                result = Int32.TryParse(searchStr, out vendorNum);
+                if (result == true)
+                {
+                    searchFilter = (v => (v.Vendnum == vendorNum) ||
+                                    (v.Company.ToLower().Contains(searchStr.ToLower())) ||
+                                    (v.Phone.Contains(searchStr) ||
+                                    (v.DBA.ToLower().Contains(searchStr.ToLower()))));
+                }
+                else
+                {
+                    searchFilter = (v => (v.Company.ToLower().Contains(searchStr.ToLower())) ||
+                                         (v.Phone.Contains(searchStr) ||
+                                        (v.DBA.ToLower().Contains(searchStr.ToLower()))));
+                }
+            }
+
+            using (UoW)
+            {
+                //Get the Insurance Types explicity loaded
+                var insTypes = UoW.InsuranceTypes.Get();
+                //Now get the Vendors
+                var vendors = UoW.Vendors.Get(out totalRowCount,
+                    filter: searchFilter,
+                    orderBy: c => c.OrderBy(v => v.Vendnum),
+                    includeProperties: "VendorInsurances, VendorRemitToes",
+                    pageSize: pageSize,
+                    currentPage: currentPage);
+
+                return Json(new { Data = vendors, InsuranceTypes = insTypes, VirtualRowCount = totalRowCount }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public JsonResult SaveClient(Client client)
+        {
+            DateTime Now = DateTime.Now;
+            string UserName = System.Web.HttpContext.Current.User.Identity.Name;
+            var result = false;
+
+            if (ModelState.IsValid)
+            {
+                using (this.UoW)
+                {
+                    if (client.ClientID == Guid.Empty)
+                    {
+                        client.Inputby = UserName;
+                        client.Inputdate = Now;
+                        client.ClientID = Guid.NewGuid();
+                        this.UoW.Clients.Insert(client);
+                    }
+                    else
+                    {
+                        client.LastModifiedBy = UserName;
+                        client.LastModifiedDate = Now;
+                        this.UoW.Clients.Update(client);
+                    }
+                    result = this.UoW.Commit() > 0;
+                }
+                return Json(new { Success = result });
+            }
+            else
+            {
+                return Json(new { Success = result, Message = "Invalid Model" });
+            }
+        }
+
+        public JsonResult DeleteClient(Guid clientID)
+        {
+            bool result = false;
+            using (this.UoW)
+            {
+                this.UoW.Clients.Delete(clientID);
+                result = this.UoW.Commit() > 0;
+            }
+            return Json(new { Success = result });
+        }
+
+        #endregion
+    }
+}
--- BroadwayNext/Controllers/ClientListingController.cs
+++ BroadwayNext/Controllers/ClientListingController.cs
@@ -16,7 +16,7 @@ namespace BroadwayNextWeb.Controllers
 {
     
     [Authorize]
-    public class ClientListingController : Controller
+    public partial class ClientListingController : Controller
     {
         //
         // GET: /ClientListing/
@@ -32,107 +32,107 @@ namespace BroadwayNextWeb.Controllers
             return View();
         }
 
-        #region Client
+        //#region Client
 
-        public JsonResult GetClients()
-        {
-            int totalRowCount;
+        //public JsonResult GetClients()
+        //{
+        //    int totalRowCount;
            
-            using (UoW)
-            {
-                var clients = UoW.Clients.Get(out totalRowCount);
-                return Json(new {Data = clients, VirtualRowCount = totalRowCount }, JsonRequestBehavior.AllowGet);               
-            }
+        //    using (UoW)
+        //    {
+        //        var clients = UoW.Clients.Get(out totalRowCount);
+        //        return Json(new {Data = clients, VirtualRowCount = totalRowCount }, JsonRequestBehavior.AllowGet);               
+        //    }
             
-        }
+        //}
 
 
-        public JsonResult GetAllClients1(int pageSize, int currentPage, string searchStr)
-        {
-            int totalRowCount;
-            Expression<Func<Vendor, bool>> searchFilter = null;
+        //public JsonResult GetAllClients1(int pageSize, int currentPage, string searchStr)
+        //{
+        //    int totalRowCount;
+        //    Expression<Func<Vendor, bool>> searchFilter = null;
 
-            if (!string.IsNullOrEmpty(searchStr))
-            {
-                int vendorNum; bool result;
-                result = Int32.TryParse(searchStr, out vendorNum);
-                if (result == true)
-                {
-                    searchFilter = (v => (v.Vendnum == vendorNum) ||
-                                    (v.Company.ToLower().Contains(searchStr.ToLower())) ||
-                                    (v.Phone.Contains(searchStr) ||
-                                    (v.DBA.ToLower().Contains(searchStr.ToLower()))));
-                }
-                else
-                {
-                    searchFilter = (v => (v.Company.ToLower().Contains(searchStr.ToLower())) ||
-                                         (v.Phone.Contains(searchStr) ||
-                                        (v.DBA.ToLower().Contains(searchStr.ToLower()))));
-                }
-            }
+        //    if (!string.IsNullOrEmpty(searchStr))
+        //    {
+        //        int vendorNum; bool result;
+        //        result = Int32.TryParse(searchStr, out vendorNum);
+        //        if (result == true)
+        //        {
+        //            searchFilter = (v => (v.Vendnum == vendorNum) ||
+        //                            (v.Company.ToLower().Contains(searchStr.ToLower())) ||
+        //                            (v.Phone.Contains(searchStr) ||
+        //                            (v.DBA.ToLower().Contains(searchStr.ToLower()))));
+        //        }
+        //        else
+        //        {
+        //            searchFilter = (v => (v.Company.ToLower().Contains(searchStr.ToLower())) ||
+        //                                 (v.Phone.Contains(searchStr) ||
+        //                                (v.DBA.ToLower().Contains(searchStr.ToLower()))));
+        //        }
+        //    }
 
-            using (UoW)
-            {
-                //Get the Insurance Types explicity loaded
-                var insTypes = UoW.InsuranceTypes.Get();
-                //Now get the Vendors
-                var vendors = UoW.Vendors.Get(out totalRowCount,
-                    filter: searchFilter,
-                    orderBy: c => c.OrderBy(v => v.Vendnum),
-                    includeProperties: "VendorInsurances, VendorRemitToes",
-                    pageSize: pageSize,
-                    currentPage: currentPage);
+        //    using (UoW)
+        //    {
+        //        //Get the Insurance Types explicity loaded
+        //        var insTypes = UoW.InsuranceTypes.Get();
+        //        //Now get the Vendors
+        //        var vendors = UoW.Vendors.Get(out totalRowCount,
+        //            filter: searchFilter,
+        //            orderBy: c => c.OrderBy(v => v.Vendnum),
+        //            includeProperties: "VendorInsurances, VendorRemitToes",
+        //            pageSize: pageSize,
+        //            currentPage: currentPage);
 
-                return Json(new { Data = vendors, InsuranceTypes = insTypes, VirtualRowCount = totalRowCount }, JsonRequestBehavior.AllowGet);
-            }
-        }
+        //        return Json(new { Data = vendors, InsuranceTypes = insTypes, VirtualRowCount = totalRowCount }, JsonRequestBehavior.AllowGet);
+        //    }
+        //}
 
 
-        public JsonResult SaveClient(Client client)
-        {
-            DateTime Now = DateTime.Now;
-            string UserName = System.Web.HttpContext.Current.User.Identity.Name;
-            var result = false;
+        //public JsonResult SaveClient(Client client)
+        //{
+        //    DateTime Now = DateTime.Now;
+        //    string UserName = System.Web.HttpContext.Current.User.Identity.Name;
+        //    var result = false;
             
-            if (ModelState.IsValid)
-            {
-                using (this.UoW)
-                {
-                    if (client.ClientID == Guid.Empty)
-                    {
-                        client.Inputby = UserName;
-                        client.Inputdate = Now;
-                        client.ClientID = Guid.NewGuid();
-                        this.UoW.Clients.Insert(client);
-                    }
-                    else
-                    {
-                        client.LastModifiedBy = UserName;
-                        client.LastModifiedDate = Now;
-                        this.UoW.Clients.Update(client);
-                    }
-                    result = this.UoW.Commit() > 0;
-                }
-                return Json(new { Success = result});
-            }
-            else
-            {
-                return Json(new { Success = result, Message = "Invalid Model" });
-            }
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        using (this.UoW)
+        //        {
+        //            if (client.ClientID == Guid.Empty)
+        //            {
+        //                client.Inputby = UserName;
+        //                client.Inputdate = Now;
+        //                client.ClientID = Guid.NewGuid();
+        //                this.UoW.Clients.Insert(client);
+        //            }
+        //            else
+        //            {
+        //                client.LastModifiedBy = UserName;
+        //                client.LastModifiedDate = Now;
+        //                this.UoW.Clients.Update(client);
+        //            }
+        //            result = this.UoW.Commit() > 0;
+        //        }
+        //        return Json(new { Success = result});
+        //    }
+        //    else
+        //    {
+        //        return Json(new { Success = result, Message = "Invalid Model" });
+        //    }
+        //}
 
-        public JsonResult DeleteClient(Guid clientID)
-        {
-            bool result = false;
-            using (this.UoW)
-            {
-                this.UoW.Clients.Delete(clientID);
-                result = this.UoW.Commit() > 0;
-            }
-            return Json(new { Success = result });
-        }
+        //public JsonResult DeleteClient(Guid clientID)
+        //{
+        //    bool result = false;
+        //    using (this.UoW)
+        //    {
+        //        this.UoW.Clients.Delete(clientID);
+        //        result = this.UoW.Commit() > 0;
+        //    }
+        //    return Json(new { Success = result });
+        //}
 
-        #endregion
+        //#endregion
 
         #region Client Instructions
 
